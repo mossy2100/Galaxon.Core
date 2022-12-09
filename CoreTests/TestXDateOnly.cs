821e8c0ea4ad695ca47ae28df551c53195fc3a15
Shaun Moss <shaun@astromultimedia.com>
2022-12-09 15:02:36 +1000

    Added tests.

---
+++ CoreTests/TestXDateOnly.cs
@@ -0,0 +1,217 @@
+﻿using AstroMultimedia.Core.Time;
+
+namespace AstroMultimedia.Core.Tests;
+
+[TestClass]
+public class TestXDateOnly
+{
+    [TestMethod]
+    public void TestDateOnlyGetTotalDays()
+    {
+        DateOnly date;
+
+        // Test start of range.
+        date = new DateOnly(1, 1, 1);
+        Assert.AreEqual(date.GetTotalDays(), 0);
+
+        // Test current date.
+        date = new DateOnly(2022, 6, 8);
+        Assert.AreEqual(date.GetTotalDays(), 738313);
+
+        // Test middle of range.
+        date = new DateOnly(5000, 7, 2);
+        Assert.AreEqual(date.GetTotalDays(), 1826029);
+
+        // Test end of range.
+        date = new DateOnly(9999, 12, 31);
+        Assert.AreEqual(date.GetTotalDays(), 3652058);
+    }
+
+    [TestMethod]
+    public void TestDateOnlyFromTotalDays()
+    {
+        DateOnly date1, date2;
+
+        date1 = new DateOnly(1, 1, 1);
+        date2 = XDateOnly.FromTotalDays(0);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        date1 = new DateOnly(2022, 6, 8);
+        date2 = XDateOnly.FromTotalDays(738313);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        date1 = new DateOnly(5000, 7, 2);
+        date2 = XDateOnly.FromTotalDays(1826029);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        date1 = new DateOnly(9999, 12, 31);
+        date2 = XDateOnly.FromTotalDays(3652058);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+    }
+
+    // TODO Include a reference to the source of the test data.
+    [TestMethod]
+    public void TestDateOnlyToJulianDay()
+    {
+        DateOnly date;
+
+        // Test start of range.
+        date = new DateOnly(1, 1, 1);
+        Assert.AreEqual(date.ToJulianDay(), 1721425.5);
+
+        // Test current date.
+        date = new DateOnly(2022, 6, 8);
+        Assert.AreEqual(date.ToJulianDay(), 2459738.5);
+
+        // Test middle of range.
+        date = new DateOnly(5000, 7, 2);
+        Assert.AreEqual(date.ToJulianDay(), 3547454.5);
+
+        // Test end of range.
+        date = new DateOnly(9999, 12, 31);
+        Assert.AreEqual(date.ToJulianDay(), 5373483.5);
+    }
+
+    // TODO Include a reference to the source of the test data.
+    [TestMethod]
+    public void TestDateOnlyFromJulianDay()
+    {
+        DateOnly date1, date2;
+
+        // Test start of range.
+        date1 = new DateOnly(1, 1, 1);
+        date2 = XDateOnly.FromJulianDay(1721425.5);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        // Test current date.
+        date1 = new DateOnly(2022, 6, 8);
+        date2 = XDateOnly.FromJulianDay(2459738.5);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        // Test middle of range.
+        date1 = new DateOnly(5000, 7, 2);
+        date2 = XDateOnly.FromJulianDay(3547454.5);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+
+        // Test end of range.
+        date1 = new DateOnly(9999, 12, 31);
+        date2 = XDateOnly.FromJulianDay(5373483.5);
+        Assert.AreEqual(date1.GetTicks(), date2.GetTicks());
+    }
+
+    [TestMethod]
+    public void TestSubtract()
+    {
+        // This uses the dates of Halley's Comet from example 7.d in AA2 p64.
+        DateOnly date1 = new DateOnly(1910, 4, 20);
+        DateOnly date2 = new DateOnly(1986, 2, 9);
+        Assert.AreEqual(date2.Subtract(date1).Days, 27689);
+    }
+
+    [TestMethod]
+    public void TestAdd()
+    {
+        // From the exercise in AA2 p64.
+        DateOnly date1 = new DateOnly(1991, 7, 11);
+        DateOnly date2 = date1.AddDays(10000);
+        Assert.AreEqual(date2.ToIsoString(), "2018-11-26");
+    }
+
+    [TestMethod]
+    // Also tests ToIsoString().
+    public void TestFromDayOfYear()
+    {
+        DateOnly date1, date2;
+        int n1, n2;
+
+        // Test start of range.
+        date1 = new DateOnly(1, 1, 1);
+        n1 = date1.DayOfYear;
+        date2 = XDateOnly.FromDayOfYear(1, n1);
+        Assert.AreEqual(date2.ToIsoString(), "0001-01-01");
+        n2 = date2.DayOfYear;
+        Assert.AreEqual(n1, n2);
+
+        // Test leap year.
+        date1 = new DateOnly(2020, 12, 31);
+        n1 = date1.DayOfYear;
+        date2 = XDateOnly.FromDayOfYear(2020, n1);
+        Assert.AreEqual(date2.ToIsoString(), "2020-12-31");
+        n2 = date2.DayOfYear;
+        Assert.AreEqual(n1, n2);
+
+        // Test current date.
+        date1 = new DateOnly(2022, 6, 8);
+        n1 = date1.DayOfYear;
+        date2 = XDateOnly.FromDayOfYear(2022, n1);
+        Assert.AreEqual(date2.ToIsoString(), "2022-06-08");
+        n2 = date2.DayOfYear;
+        Assert.AreEqual(n1, n2);
+
+        // Test middle of range.
+        date1 = new DateOnly(5000, 7, 2);
+        n1 = date1.DayOfYear;
+        date2 = XDateOnly.FromDayOfYear(5000, n1);
+        Assert.AreEqual(date2.ToIsoString(), "5000-07-02");
+        n2 = date2.DayOfYear;
+        Assert.AreEqual(n1, n2);
+
+        // Test end of range.
+        date1 = new DateOnly(9999, 12, 31);
+        n1 = date1.DayOfYear;
+        date2 = XDateOnly.FromDayOfYear(9999, n1);
+        Assert.AreEqual(date2.ToIsoString(), "9999-12-31");
+        n2 = date2.DayOfYear;
+        Assert.AreEqual(n1, n2);
+    }
+
+    /// <summary>
+    /// These examples are from Meeus AA2 p67.
+    /// I've also verified the dates at:
+    /// <see href="https://www.assa.org.au/edm" />
+    /// </summary>
+    [TestMethod]
+    public static void TestEaster()
+    {
+        DateOnly date;
+
+        date = XDateOnly.Easter(1991);
+        Assert.AreEqual(date.ToIsoString(), "1991-03-31");
+
+        date = XDateOnly.Easter(1992);
+        Assert.AreEqual(date.ToIsoString(), "1992-04-19");
+
+        date = XDateOnly.Easter(1993);
+        Assert.AreEqual(date.ToIsoString(), "1993-04-11");
+
+        date = XDateOnly.Easter(1954);
+        Assert.AreEqual(date.ToIsoString(), "1954-04-18");
+
+        date = XDateOnly.Easter(2000);
+        Assert.AreEqual(date.ToIsoString(), "2000-04-23");
+
+        date = XDateOnly.Easter(1818);
+        Assert.AreEqual(date.ToIsoString(), "1818-03-22");
+
+        date = XDateOnly.Easter(2285);
+        Assert.AreEqual(date.ToIsoString(), "2285-03-22");
+
+        date = XDateOnly.Easter(1886);
+        Assert.AreEqual(date.ToIsoString(), "1886-04-25");
+
+        date = XDateOnly.Easter(1943);
+        Assert.AreEqual(date.ToIsoString(), "1943-04-25");
+
+        date = XDateOnly.Easter(2038);
+        Assert.AreEqual(date.ToIsoString(), "2038-04-25");
+    }
+
+    /// <summary>
+    /// Tests the Easter dates from the database (1600-2299).
+    /// TODO Update to use a data file.
+    /// </summary>
+    // [TestMethod]
+    public static void TestEaster2()
+    {
+    }
+}
---
+++ CoreTests/TestXDateTime.cs
@@ -0,0 +1,121 @@
+﻿using AstroMultimedia.Core.Time;
+using static System.DateTimeKind;
+
+namespace AstroMultimedia.Core.Tests;
+
+[TestClass]
+public class TestXDateTime
+{
+    private const double _DELTA = 1e-9;
+
+    [TestMethod]
+    public void TestDateTimeGetTotalDays()
+    {
+        DateTime dt;
+
+        // Test start of range.
+        dt = new DateTime(1, 1, 1, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.GetTotalDays(), 0);
+
+        // Test current date.
+        dt = new DateTime(2022, 6, 8, 5, 50, 24, 0, Utc);
+        Assert.AreEqual(dt.GetTotalDays(), 738313.243333333, _DELTA);
+
+        // Test middle of range.
+        dt = new DateTime(5000, 7, 2, 12, 30, 0, 0, Utc);
+        Assert.AreEqual(dt.GetTotalDays(), 1826029.520833333, _DELTA);
+
+        // Test end of range.
+        dt = new DateTime(9999, 12, 31, 23, 59, 59, 999, Utc);
+        Assert.AreEqual(dt.GetTotalDays(), 3652058.999999988, _DELTA);
+    }
+
+    [TestMethod]
+    public void TestDateTimeFromTotalDays()
+    {
+        DateTime dt1, dt2;
+        long epsilon = 1000;
+
+        dt1 = new DateTime(1, 1, 1, 0, 0, 0, 0, Utc);
+        dt2 = XDateTime.FromTotalDays(0);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks);
+
+        dt1 = new DateTime(2022, 6, 8, 5, 50, 24, 0, Utc);
+        dt2 = XDateTime.FromTotalDays(738313.24333333333333);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks, epsilon);
+
+        dt1 = new DateTime(5000, 7, 2, 12, 30, 0, 0, Utc);
+        dt2 = XDateTime.FromTotalDays(1826029.52083333333333);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks, epsilon);
+
+        dt1 = new DateTime(9999, 12, 31, 23, 59, 59, 999, Utc);
+        dt2 = XDateTime.FromTotalDays(3652058.99999998842593);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks, epsilon);
+    }
+
+    // TODO Include a reference to the source of the test data.
+    [TestMethod]
+    public void TestDateTimeToJulianDay()
+    {
+        DateTime dt;
+
+        // Test start of range.
+        dt = new DateTime(1, 1, 1, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 1721425.5);
+
+        // Test current date.
+        dt = new DateTime(2022, 6, 7, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2459737.5);
+
+        // Test middle of range.
+        dt = new DateTime(5000, 7, 2, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 3547454.5);
+
+        // Test end of range.
+        dt = new DateTime(9999, 12, 31, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 5373483.5);
+
+        // Test values from Meeus p62.
+        dt = new DateTime(2000, 1, 1, 12, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2451545.0);
+        dt = new DateTime(1999, 1, 1, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2451179.5);
+        dt = new DateTime(1987, 1, 27, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2446822.5);
+        dt = new DateTime(1987, 6, 19, 12, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2446966.0);
+        dt = new DateTime(1988, 1, 27, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2447187.5);
+        dt = new DateTime(1988, 6, 19, 12, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2447332.0);
+        dt = new DateTime(1900, 1, 1, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2415020.5);
+        dt = new DateTime(1600, 1, 1, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2305447.5);
+        dt = new DateTime(1600, 12, 31, 0, 0, 0, 0, Utc);
+        Assert.AreEqual(dt.ToJulianDay(), 2305812.5);
+    }
+
+    // TODO Include a reference to the source of the test data.
+    [TestMethod]
+    public void TestDateTimeFromJulianDay()
+    {
+        DateTime dt1, dt2;
+
+        dt1 = new DateTime(1, 1, 1, 0, 0, 0, 0, Utc);
+        dt2 = XDateTime.FromJulianDay(1721425.5);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks);
+
+        dt1 = new DateTime(2022, 6, 7, 0, 0, 0, 0, Utc);
+        dt2 = XDateTime.FromJulianDay(2459737.5);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks);
+
+        dt1 = new DateTime(5000, 7, 2, 0, 0, 0, 0, Utc);
+        dt2 = XDateTime.FromJulianDay(3547454.5);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks);
+
+        dt1 = new DateTime(9999, 12, 31, 0, 0, 0, 0, Utc);
+        dt2 = XDateTime.FromJulianDay(5373483.5);
+        Assert.AreEqual(dt1.Ticks, dt2.Ticks);
+    }
+}
