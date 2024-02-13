952150c9376603e1efc50764a1698bf88afc8c81
Shaun Moss <shaun@astromultimedia.com>
2024-02-13 19:40:03 +1000

    Refactoring.

--- Core/Numbers/Sexagesimal.cs
+++ Core/Numbers/Sexagesimal.cs
@@ -40,7 +40,8 @@ public static class Sexagesimal
     {
         if (digit >= BASE)
         {
-            throw new ArgumentOutOfRangeException(nameof(digit), $"Must be in the range 0-{BASE - 1}.");
+            throw new ArgumentOutOfRangeException(nameof(digit),
+                $"Must be in the range 0-{BASE - 1}.");
         }
 
         BigInteger q, r;
@@ -63,64 +64,77 @@ public static class Sexagesimal
     }
 
     /// <summary>
-    /// Convert a decimal value into a hours, minutes, and seconds.
-    /// This can also be used to convert an angle from decimal degrees to integer degrees,
-    /// arcminutes, and arcseconds.
-    /// The hours part will be an integer.
-    /// The minutes part will be a sexagesimal digit, i.e. an integer in the range 0-59.
+    /// Convert a decimal value into units, minutes, and seconds.
+    /// This can be used to convert a decimal number of hours to integer hours with minutes and
+    /// seconds, or an angle in decimal degrees to integer degrees, arcminutes, and arcseconds.
+    /// The units part will be a signed integer, not limited to the range 0-59.
+    /// The minutes part will be an integer in the range 0-59.
     /// The seconds part will be a decimal in the range 0-59.999...
     /// The minutes and seconds values will have the same sign as the whole number, or be
     /// zero.
     /// </summary>
-    /// <param name="decimalHours">
+    /// <param name="n">
     /// The decimal value to convert. This could be a time in hours or an angle in degrees.
     /// </param>
     /// <returns>
-    /// A tuple containing values representing the hours (or degrees), minutes (or arcminutes),
-    /// and seconds (or arcseconds).
+    /// A tuple containing values representing the units (hours or degrees), minutes (or
+    /// arcminutes), and seconds (or arcseconds).
     /// </returns>
-    public static (long, sbyte, decimal) ToHoursMinutesSeconds(decimal decimalHours)
+    public static (long, sbyte, decimal) ToUnitsMinutesSeconds(decimal n)
     {
-        // Calculate the hours part.
+        // Calculate the units part.
         // This will throw an exception if the truncated value of n is outside the valid range for
-        // long. We could make hours a BigInteger but that seems unnecessary at this stage.
-        long hours = (long)decimal.Truncate(decimalHours);
+        // long. We could make units a BigInteger but that seems unnecessary at this stage.
+        long units = (long)decimal.Truncate(n);
 
         // Calculate the minutes part.
-        decimal decimalMinutes = (decimalHours - hours) * BASE;
+        decimal decimalMinutes = (n - units) * BASE;
         sbyte minutes = (sbyte)decimal.Truncate(decimalMinutes);
 
         // Calculate the seconds part.
         decimal seconds = (decimalMinutes - minutes) * BASE;
 
-        return (hours, minutes, seconds);
+        return (units, minutes, seconds);
     }
 
-    /// <summary>Convert a decimal value to integer, minutes, and seconds notation.</summary>
-    /// <param name="decimalHours">
+    /// <summary>
+    /// Convert a value provided as units (e.g. hours or degrees), minutes and seconds, to a decimal
+    /// value.
+    /// </summary>
+    /// <param name="units"></param>
+    /// <param name="minutes"></param>
+    /// <param name="seconds"></param>
+    /// <returns></returns>
+    public static decimal FromUnitsMinutesSeconds(decimal units, decimal minutes, decimal seconds)
+    {
+        return units + (minutes * BASE) + (seconds * BASE * BASE);
+    }
+
+    /// <summary>Convert a decimal value to units, minutes, and seconds notation.</summary>
+    /// <param name="n">
     /// The decimal value to convert. This could be a time in hours or an angle in degrees.
     /// </param>
     /// <param name="notation">The notation to use for the output string.</param>
     /// <returns>The argument as a string formatted using the specified notation.</returns>
-    public static string ToString(decimal decimalHours,
+    public static string ToString(decimal n,
         ESexagesimalNotation notation = ESexagesimalNotation.Angle)
     {
         // Handle negative values.
-        if (decimalHours < 0)
+        if (n < 0)
         {
-            return '-' + ToString(-decimalHours);
+            return '-' + ToString(-n);
         }
 
-        // Convert decimal value to hours, minutes, and seconds.
-        (long hours, sbyte minutes, decimal seconds) = ToHoursMinutesSeconds(decimalHours);
+        // Convert decimal value to units, minutes, and seconds.
+        (long units, sbyte minutes, decimal seconds) = ToUnitsMinutesSeconds(n);
 
         // Format the output.
         return notation switch
         {
-            ESexagesimalNotation.Angle => $"{hours}°{minutes}′{seconds}″",
-            ESexagesimalNotation.Colons => $"{hours}:{minutes}:{seconds}",
-            ESexagesimalNotation.TimeUnits => $"{hours}h {minutes}m {seconds}s",
-            ESexagesimalNotation.Neugebauer => $"{hours};{minutes},{seconds}",
+            ESexagesimalNotation.Angle => $"{units}°{minutes}′{seconds}″",
+            ESexagesimalNotation.Colons => $"{units}:{minutes}:{seconds}",
+            ESexagesimalNotation.TimeUnits => $"{units}h {minutes}m {seconds}s",
+            ESexagesimalNotation.Neugebauer => $"{units};{minutes},{seconds}",
             _ => throw new ArgumentOutOfRangeException(nameof(notation), "Invalid notation.")
         };
     }
